3bbdd0dfb62dfdc7891b28980776f6c65ee7b898
starcradle101 <dreamerdev12@gmail.com>
2024-12-29 17:15:03 +0900

    chore(init): 프로젝트 초기 환경 설정 추가
    - 에셋 파일 이동
    - 카카오톡 공유하기 아이콘 추가
    - NotFoundPage 컴포넌트 디자인 일부 수정
    - Kakao SDK 타입 의존성 추가

--- src/pages/NotFoundPage.tsx
+++ src/pages/NotFoundPage.tsx
@@ -6,10 +6,10 @@ export const NotFoundPage = () => {
   return (
     <div className='flex min-h-screen flex-col items-center justify-center'>
       <h1 className='text-4xl font-bold'>404</h1>
-      <p className='mt-2'>페이지를 찾을 수 없습니다</p>
+      <p className='mt-2 text-xl'>페이지를 찾을 수 없습니다</p>
       <button
         onClick={() => navigate("/")}
-        className='mt-4 rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600'
+        className='mt-4 rounded bg-emerald-500 px-4 py-2 text-lg text-white hover:bg-emerald-600'
       >
         메인으로
       </button>
